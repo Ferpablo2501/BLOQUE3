8af92484a93565d7e285393a04ac2d482e22984a
ferpablo24 <areyes@ugb.edu.sv>
2024-10-30 17:35:12 -0600

    Agregar archivos de proyecto.

---
+++ ISNP106424-ISNP212323_BLOQUE3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISNP106424_ISNP212323_BLOQUE3.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ ISNP106424-ISNP212323_BLOQUE3/Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISNP106424_ISNP212323_BLOQUE3.Models
+{
+	public class Pelicula
+	{
+		[Key]
+		public int IdPelicula	{ get; set; }
+		public String Titulo { get; set; }
+		public String Director { get; set; }
+		public String Sinopsis { get; set; }
+		public String Duracion { get; set; }
+		public String Clasificacion { get; set; }
+	}
+}
